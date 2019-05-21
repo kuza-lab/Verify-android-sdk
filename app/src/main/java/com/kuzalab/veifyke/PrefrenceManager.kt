af1d0181c8b7a333ad757b38d3672c3d6fd106c5
Erick Kogi <kogi@kuzalab.com>
2019-05-21 14:06:28 +0300

    Preferences manager to save production keys

--- app/src/main/java/com/kuzalab/veifyke/PrefrenceManager.kt
+++ app/src/main/java/com/kuzalab/veifyke/PrefrenceManager.kt
@@ -1,8 +1,8 @@
 /*
  * *
- *  * Created by Kogi Eric  on 5/21/19 1:57 PM
+ *  * Created by Kogi Eric  on 5/21/19 2:06 PM
  *  * Copyright (c) 2019 . All rights reserved.
- *  * Last modified 5/21/19 1:57 PM
+ *  * Last modified 5/21/19 2:06 PM
  *
  */
 
@@ -11,16 +11,15 @@ package com.kuzalab.veifyke
 import android.content.Context
 import android.content.SharedPreferences
 
-class PrefrenceManager(internal var _context: Context) {
-    internal var pref: SharedPreferences
-    internal var editor: SharedPreferences.Editor
-    internal var PRIVATE_MODE = 0
+class PrefrenceManager(_context: Context) {
+    private var pref: SharedPreferences
+    private var editor: SharedPreferences.Editor
+    private var PRIVATE_MODE = 0
 
     companion object {
-        private val SECRETKEY = "secretkey"
-        private val CONSUMERKEY = "consumerkey"
-
-        private val PREF_NAME = "verify_ke_prefrences"
+        private const val SECRETKEY = "secretkey"
+        private const val CONSUMERKEY = "consumerkey"
+        private const val PREF_NAME = "verify_ke_prefrences"
     }
 
     init {
--- verifysdk/src/main/java/com/kuzalab/verifysdk/network/BaseUrls.kt
+++ verifysdk/src/main/java/com/kuzalab/verifysdk/network/BaseUrls.kt
@@ -1,16 +1,16 @@
 /*
  * *
- *  * Created by Kogi Eric  on 5/20/19 5:42 PM
+ *  * Created by Kogi Eric  on 5/21/19 2:06 PM
  *  * Copyright (c) 2019 . All rights reserved.
- *  * Last modified 5/20/19 3:58 PM
+ *  * Last modified 5/21/19 2:06 PM
  *
  */
 
 internal class BaseUrls {
 
 
-    var SANDBOX: String = "https://sandbox.verify.ke/"
-    var LIVE: String = "https://api.verify.ke/"
+    val SANDBOX: String = "https://sandbox.verify.ke/"
+    val LIVE: String = "https://api.verify.ke/"
 
 
 }
